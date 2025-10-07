3c59dc07d49a5feaf75f6689c2f9ee37399b79f2
Cuong 24520239 <cuongvn852654@gmail.com>
2025-10-07 18:30:58 +0700

    Add files via upload

---
+++ C++/bai11/bai11.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char a; cout<<"nhap 1 ky tu: "; cin>>a;
+    int n=a;
+    cout<<"so nguyen tuong ung trong bang ASCII la: "<<n;
+    return 0;
+}
---
+++ C++/bai12/Bai12.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n; cout<<"nhap 1 so nguyen: "; cin>>n;
+    char a=n;
+    cout<<"ky tu tuong ung trong bang ASCII la: "<<a;
+    return 0;
+}
